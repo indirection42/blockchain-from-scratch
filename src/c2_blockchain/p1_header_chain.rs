57de236272f6e923a334c8dd2ec0dff4af209a11
indirection42 <jiyuanz95@gmail.com>
2024-04-29 16:35:10 +0800

    feat(blockchain): p1 finished

--- src/c2_blockchain/p1_header_chain.rs
+++ src/c2_blockchain/p1_header_chain.rs
@@ -24,12 +24,18 @@ pub struct Header {
 impl Header {
 	/// Returns a new valid genesis header.
 	fn genesis() -> Self {
-		todo!("Exercise 1")
+		Header { parent: 0, height: 0, extrinsics_root: (), state_root: (), consensus_digest: () }
 	}
 
 	/// Create and return a valid child header.
 	fn child(&self) -> Self {
-		todo!("Exercise 2")
+		Header {
+			parent: hash(self),
+			height: self.height + 1,
+			extrinsics_root: (),
+			state_root: (),
+			consensus_digest: (),
+		}
 	}
 
 	/// Verify that all the given headers form a valid chain from this header to the tip.
@@ -37,7 +43,22 @@ impl Header {
 	/// This method may assume that the block on which it is called is valid, but it
 	/// must verify all of the blocks in the slice;
 	fn verify_sub_chain(&self, chain: &[Header]) -> bool {
-		todo!("Exercise 3")
+		if chain.len() == 1 {
+			if chain[0].parent == hash(&Header::genesis()) && chain[0].height != 1 {
+				return false;
+			}
+			if chain[0].height == 1 && chain[0].parent != hash(&Header::genesis()) {
+				return false;
+			}
+		}
+		for window in chain.windows(2) {
+			let parent = &window[0];
+			let child = &window[1];
+			if child.parent != hash(&parent) || child.height != parent.height + 1 {
+				return false;
+			}
+		}
+		true
 	}
 }
 
@@ -45,14 +66,27 @@ impl Header {
 
 /// Build and return a valid chain with exactly five blocks including the genesis block.
 fn build_valid_chain_length_5() -> Vec<Header> {
-	todo!("Exercise 4")
+	let mut chain = Vec::new();
+	for _ in 0..5 {
+		chain.push(chain.last().map_or_else(Header::genesis, |h: &Header| h.child()));
+	}
+	chain
 }
 
 /// Build and return a chain with at least three headers.
 /// The chain should start with a proper genesis header,
 /// but the entire chain should NOT be valid.
 fn build_an_invalid_chain() -> Vec<Header> {
-	todo!("Exercise 5")
+	let mut chain = vec![Header::genesis()];
+	chain.push(chain.last().unwrap().child());
+	chain.push(Header {
+		parent: 5,
+		height: 2,
+		extrinsics_root: (),
+		state_root: (),
+		consensus_digest: (),
+	});
+	chain
 }
 
 // To run these tests: `cargo test bc_1
